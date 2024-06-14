c0a1c60253fc64a531b06aef2c47d9785a3dc965
srividhyasivasamy <srividhyasoftwaretesting@gmail.com>
2024-06-14 15:16:44 +0530

    S.Srividhya_Nadarsaraswathicollegeofarts&science
    Assessment-2

---
+++ NSCAS-1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NSCAS-1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ada89a10-7f63-4796-9c74-8f0e0933f3b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>a8122517-e899-42f4-9176-d1de9c03bd7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NSCAS/Home</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
